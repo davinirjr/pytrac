0bd5b884dcc2e22d17620a35877c72ecae25027a
Daniel Bonkowski <bonko@jimdo.com>
2013-12-20 12:49:29 +0100

    initial experimental stuff

---
+++ pytrac/ticket.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import xmlrpclib
+
+
+class Ticket():
+
+    def __init__(self, user, host, password):
+        self.url = 'https://%s:%s@%s/trac/login/xmlrpc' % (user, password, host)
+        self.server = xmlrpclib.ServerProxy(self.url)
+        self.api = self.server.ticket
+
+    def search(self, query):
+        result = self.api.query(query)
+        print result
+
+
+#
+#def update():
+#    print "dummy"
+#
+#
+#def create():
+#    print "dummy"
