33a0d58185584c8be529401582e8039263756d1c
Daniel Bonkowski <bonko@jimdo.com>
2013-12-20 13:35:26 +0100

    search with params

--- pytrac/ticket.py
+++ pytrac/ticket.py
@@ -1,17 +1,33 @@
 import xmlrpclib
 
 
-class Ticket(Object):
+class Ticket(object):
 
     def __init__(self, user, host, password):
         self.url = 'https://%s:%s@%s/trac/login/xmlrpc' % (user, password, host)
         self.server = xmlrpclib.ServerProxy(self.url)
         self.api = self.server.ticket
 
-    def search(self, query):
+    def search_raw(self, query):
         result = self.api.query(query)
         print result
 
+    def search(self, summary=None, owner=None, status=None):
+        query = ''
+        if summary:
+            query += "summary~=%s&" % summary
+        if owner:
+            query += "owner=%s&" % owner
+        if status:
+            query += "status=%s&" % status
+        if query == '':
+            raise Exception("Query empty!")
+        query = query.rstrip('&')
+        print '"%s"' % query
+        result = self.api.query(query)
+        print result
+
+#    def search(self, query):
 
 #
 #def update():
