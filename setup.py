e4fd9cde2aa65b8030b759988bd5477c892887be
Ilya Margolin <ilya@jimdo.com>
2014-01-31 13:01:35 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='pytrac',
+      version='0.0.1',
+      author='Daniel Bonkowski',
+      author_email='bonko@jimdo.com',
+      url='https://github.com/Jimdo/pytrac',
+      license='Apache',
+      packages=['pytrac'],
+      tests_require=['mock'],
+      test_suite='test',
+      )
