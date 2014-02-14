03db6c12584652230fe0cd1f982f2a70a7c1630b
Pirmin Fix <pirmin.fix@jimdo.com>
2014-02-14 14:33:58 +0100

    Add test for comment api

--- test/test_ticket.py
+++ test/test_ticket.py
@@ -2,6 +2,7 @@ import unittest
 from mock import Mock
 import sys
 import os
+import datetime
 
 from pytrac import Ticket
 
@@ -15,6 +16,30 @@ class TestTicket(unittest.TestCase):
     def testSearchWithAllParams(self):
         self.ticket.search(summary='test_summary', owner='someowner', status='new')
         self.ticket.api.query.assert_called_with('max=0&summary~=test_summary&owner=someowner&status=new')
+
+
+class TestUpdateTicket(unittest.TestCase):
+
+    ticket_id = 1
+
+    def setUp(self):
+        server = Mock()
+        timestamp = datetime.datetime.now()
+        server.ticket.get.return_value = [self.ticket_id,
+                                          timestamp,
+                                          timestamp,
+                                          {'_ts': timestamp,
+                                           'action': 'leave'}]
+        server.ticket.update.return_value = [self.ticket_id,
+                                             timestamp,
+                                             timestamp,
+                                             {'_ts': timestamp,
+                                              'action': 'leave'}]
+        self.ticket = Ticket(server)
+
+    def testComment(self):
+        self.ticket.comment(self.ticket_id, "some comment")
+        self.ticket.api.get.assert_called_with(1)
 
 
 if __name__ == '__main__':
