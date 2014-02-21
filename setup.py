222bbea8838d3eed7f0510e0b0ded03116701e85
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2014-02-21 12:23:27 +0100

    Convert md documentation to rst using `setup.py doc`

--- setup.py
+++ setup.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, Command
+
+class PyPandoc(Command):
+    description = 'Generates the documentation in reStructuredText format.'
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def convert(self, infile, outfile):
+        import pypandoc
+        with open(outfile, 'w+') as f:
+            f.write(pypandoc.convert(infile, 'rst'))
+
+    def run(self):
+        self.convert('README.md', 'rst/README.rst')
+        self.convert('HISTORY.md', 'rst/HISTORY.rst')
+
 
 setup(name='pytrac',
       version='0.0.1',
@@ -8,7 +28,11 @@ setup(name='pytrac',
       author_email='bonko@jimdo.com',
       url='https://github.com/Jimdo/pytrac',
       license='Apache',
+      description='Library for managing TRAC tickets with Python',
+      long_description=open('rst/README.rst').read() + '\n\n' +
+                       open('rst/HISTORY.rst').read(),
       packages=['pytrac'],
       tests_require=['mock'],
       test_suite='test',
+      cmdclass={'doc': PyPandoc},
       )
