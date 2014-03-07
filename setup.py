8c97d16f5e2b62aa72dba79775ed5c051449bd12
Daniel Bonkowski <bonko@jimdo.com>
2014-03-07 16:20:44 +0100

    raised version to 0.1.1

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ class PyPandoc(Command):
 
 
 setup(name='pytrac',
-      version='0.1',
+      version='0.1.1',
       author='Daniel Bonkowski, Pirmin Fix',
       author_email='bonko@jimdo.com, pirmin.fix@jimdo.com',
       url='https://github.com/Jimdo/pytrac',
