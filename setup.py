6f05ab4ce2ff544b59a8687b2d5a1c64fa541c64
Daniel Bonkowski <bonko@jimdo.com>
2014-08-15 11:46:40 +0200

    require backoff 1.0.4 for python 2.6 support and install it on travis

--- setup.py
+++ setup.py
@@ -53,5 +53,5 @@ setup(name='pytrac',
       tests_require=['pytest', 'mock'],
       test_suite='test',
       cmdclass={'test': PyTest, 'doc': PyPandoc},
-      install_requires=['backoff'],
+      install_requires=['backoff >= 1.0.4'],
       )
